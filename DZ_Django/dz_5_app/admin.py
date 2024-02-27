9706124a9894a0ef36803c195fc60f1b70d896e5
Lyudmila <lyudmila.kostornova@mail.ru>
2024-02-27 23:43:44 +0300

    Задание 5

--- DZ_Django/dz_5_app/admin.py
+++ DZ_Django/dz_5_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.action(description="Сбросить количество в ноль")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'quantity']
+    ordering = ['category', '-quantity']
+    list_filter = ['date_added', 'price']
+    search_fields = ['description']
+    search_help_text = 'Поиск по полю Описание продукта (description)'
+    actions = [reset_quantity]
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
