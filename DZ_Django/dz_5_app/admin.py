99702682f7b5c0b36e7ae7d3c4d5a6084f710056
Lyudmila <lyudmila.kostornova@mail.ru>
2024-02-28 01:35:39 +0300

    Задание 5

--- DZ_Django/dz_5_app/admin.py
+++ DZ_Django/dz_5_app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Client, Order, OrderProduct
+
 
 @admin.action(description="Сбросить количество в ноль")
 def reset_quantity(modeladmin, request, queryset):
     queryset.update(quantity=0)
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'category', 'price', 'quantity']
@@ -13,5 +15,32 @@ class ProductAdmin(admin.ModelAdmin):
     search_help_text = 'Поиск по полю Описание продукта (description)'
     actions = [reset_quantity]
 
+    readonly_fields = ['date_added', 'rating']
+    fieldsets = [(None, {'classes': ['wide'], 'fields': ['name'], }, ), ('Подробности', {'classes': ['collapse'], 'description': 'Категория товара и его подробное описание', 'fields': ['category', 'description'], },),
+                 ('Бухгалтерия', {'fields': ['price', 'quantity'], }), ('Рейтинг и прочее', {'description': 'Рейтинг сформирован автоматически на основе оценок покупателей', 'fields': ['rating', 'date_added'], }),]
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone_number', 'address', 'registration_date']
+    list_filter = ['registration_date']
+    search_fields = ['name', 'email']
+    ordering = ['registration_date']
+    readonly_fields = ['registration_date']
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'total_amount', 'order_date']
+    list_filter = ['order_date']
+    search_fields = ['client__name']
+    ordering = ['-order_date']
+    readonly_fields = ['order_date']
+    inlines = [OrderProductInline]
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
--- DZ_Django/dz_5_app/models.py
+++ DZ_Django/dz_5_app/models.py
@@ -15,3 +15,31 @@ class Product(models.Model):
     rating = models.DecimalField(default=5.0, max_digits=3, decimal_places=2)
     def __str__(self):
         return self.name
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField()
+    registration_date = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # Общая сумма заказа
+    order_date = models.DateField(auto_now_add=True)
+    
+    def str(self):
+        return f'Order for {self.client}'
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=1)
+    def save(self, *args, **kwargs):
+        self.price_per_item = self.product.price
+        self.total_price = self.quantity * self.price_per_item
+        self.order.total_amount += self.total_price
+        self.order.save()
+        super(OrderProduct, self).save(*args, **kwargs)
